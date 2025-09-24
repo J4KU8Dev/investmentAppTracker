bb161c58a34bae1bf738596515b8b7db8c7e3f25
Jakub <kubarafalski240703@gmail.com>
2025-09-24 19:50:52 +0200

    injected service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { UserInput } from "./user-input/user-input";
-import { type InvestmentInput } from './investment-input.model';
 import { InvestmentResult } from "./investment-result/investment-result";
 
 @Component({
@@ -10,35 +9,4 @@ import { InvestmentResult } from "./investment-result/investment-result";
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  resultsData?: {
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number, 
-  }[];
-  calculateInvestmentResults(data: InvestmentInput) {
-  const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
-  const annualData = [];
-  let investmentValue = initialInvestment;
-
-  for (let i = 0; i < duration; i++) {
-    const year = i + 1;
-    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    investmentValue += interestEarnedInYear + annualInvestment;
-    const totalInterest =
-      investmentValue - annualInvestment * year - initialInvestment;
-    annualData.push({
-      year: year,
-      interest: interestEarnedInYear,
-      valueEndOfYear: investmentValue,
-      annualInvestment: annualInvestment,
-      totalInterest: totalInterest,
-      totalAmountInvested: initialInvestment + annualInvestment * year,
-    });
-  }
-  this.resultsData = annualData;
-}
-}
+export class App {}
